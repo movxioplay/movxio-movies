b46754200ac539938bdfc3f23b0ca3933ce95b76
movxioplay <movxioplay@gmail.com>
2026-05-21 05:01:48 -0400

    Update service worker domain and zone ID

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10866978
+    "domain": "5gvci.com",
+    "zoneId": 11036922
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
